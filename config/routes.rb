f4bfab79c6bd22c299dbcc0afe3bdbaa02c3c5e8
Tien Yuan <tien.yuan@gmail.com>
2014-09-15 14:03:32 -0700

    Added create posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
+
+  resources :posts
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
