e9a2e0fbb0e4222ed0cf3bd1ecc4c72824c7a523
Tien Yuan <tien.yuan@gmail.com>
2014-09-18 22:27:17 +0000

    removing form_group_tag method from this version that uses simple form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
-module ApplicationHelper
-  def form_group_tag(errors, &block)
-    if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
-    else
-      content_tag :div, capture(&block), class: 'form-group'
-    end
-  end
-  
+module ApplicationHelper 
   def my_name
     "Tien Yuan"
   end
