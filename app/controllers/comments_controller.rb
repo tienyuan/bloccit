b83eb9f43f3410678ee6ac6175dbf1f3bf86a2bc
Tien Yuan <tien.yuan@gmail.com>
2014-09-19 23:34:26 +0000

    Added comment validation, but can't pull out specific errors yet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,15 @@ class CommentsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.new(comment_params)  # This line might be a problem?  
+    @comment = current_user.comments.new(comment_params)
     @comment.post = @post
+    authorize @comment
     if @comment.save
       flash[:notice] = "Comment saved sucessfully."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      render :new
     end
+    redirect_to [@topic, @post]
   end
 
   private
