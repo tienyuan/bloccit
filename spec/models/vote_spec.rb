196ccc71f20942c0e9566c9c8afe0f38221b60b0
Tien Yuan <tien.yuan@gmail.com>
2014-09-22 05:09:36 +0000

    changing vote value again

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,7 +2,7 @@ describe Vote do
   describe "validations" do
     describe "value validations" do
       it "only allows -1 or 1 as values" do
-        expect( Vote.new == (value: 1) || (value: -1) ).to eq(true)
+        expect( Vote.value == 1 || Vote.value == -1 ).to eq(true)
     end
   end
 end
