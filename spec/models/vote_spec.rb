29ef67abf0fdb16fa9a5130ea10e977de03a538a
Tien Yuan <tien.yuan@gmail.com>
2014-09-22 15:04:21 +0000

    fixing rspec test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,8 +1,27 @@
+require 'rails_helper'
+
 describe Vote do
   describe "validations" do
-    describe "value validations" do
-      it "only allows -1 or 1 as values" do
-        expect( Vote.value == 1 || Vote.value == -1 ).to eq(true)
+    before do
+      v = Vote.new(value: 1)
+      v2 = Vote.new(value: -1)
+      v3 = Vote.new(value: 2)
     end
+
+    describe "#allows 1" do
+      it "only allows -1" do
+        expect( v.valid? ).to eq(true)
+      end
+    end
+    describe "#allows -1" do
+      it "only allows -1" do
+        expect( v2.valid? ).to eq(true)
+      end
+    end
+    describe "#rejects bad votes" do
+      it "doesn't allow 2" do
+        expect( v3.valid? ).to eq(false)
+      end
+    end    
   end
 end
