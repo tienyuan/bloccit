eccebd2eac0e26d8a1adf18d1cafaaa78fe68d5a
Tien Yuan <tien.yuan@gmail.com>
2014-09-23 07:54:51 +0000

    made voting dry and added authorization

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Bloccit::Application.routes.draw do
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
 
-    post '/up-vote' => 'votes#up-vote', as: :up_vote
+    post '/up_vote' => 'votes#up_vote', as: :up_vote
     post '/down_vote' => 'votes#down_vote', as: :down_vote
   end
   
