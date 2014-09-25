85c786bb4f17cfd664d0ad91e23fc71b3821451f
Tien Yuan <tien.yuan@gmail.com>
2014-09-25 23:16:17 +0000

    added test and fixed problem in initializing comment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe "Visiting profiles" do
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
   
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A Comment")
+    @comment = Comment.new(post: @post, user: @user, body: "A Comment")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
   end
@@ -21,4 +23,17 @@ describe "Visiting profiles" do
       expect( page ).to have_content(@comment.body)
     end
   end
+  
+  describe "signed in" do
+    it "shows profile" do
+      login_as(@user, :scope => :user)
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
+
 end
