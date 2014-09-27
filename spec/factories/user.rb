22fa9fbc96aa289c14714b40be76494b5065109c
Tien Yuan <tien.yuan@gmail.com>
2014-09-27 06:55:22 +0000

    trying to add another factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,12 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+    
+    factory :user_with_post_and_comment do
+      after(:create) do |instance|
+        association :post, user: instance
+        association :comment, user: instance
+      end
+    end
   end
 end
